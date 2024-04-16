ca0f9854a53239af816fb07e3427a9de2f2ef99e
freeislet <freeislet@gmail.com>
2024-04-16 16:39:42 +0900

    cell toolbar 예제

--- src/index.ts
+++ src/index.ts
@@ -1,109 +1,33 @@
 import {
   JupyterFrontEnd,
-  JupyterFrontEndPlugin,
-  ILayoutRestorer
+  JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 import { ISettingRegistry } from '@jupyterlab/settingregistry';
-import {
-  ICommandPalette,
-  MainAreaWidget,
-  WidgetTracker
-} from '@jupyterlab/apputils';
-import { Widget } from '@lumino/widgets';
+import { INotebookTracker } from '@jupyterlab/notebook';
+// import { ICommandPalette } from '@jupyterlab/apputils';
+// import { Widget } from '@lumino/widgets';
+import { markdownIcon, runIcon } from '@jupyterlab/ui-components';
 
-interface APODResponse {
-  copyright: string;
-  date: string;
-  explanation: string;
-  media_type: 'video' | 'image';
-  title: string;
-  url: string;
-}
-
-class APODWidget extends Widget {
+const CommandIds = {
   /**
-   * Construct a new APOD widget.
+   * Command to render a markdown cell.
    */
-  constructor() {
-    super();
-
-    this.addClass('my-apodWidget');
-
-    // Add an image element to the panel
-    this.img = document.createElement('img');
-    this.node.appendChild(this.img);
-
-    // Add a summary element to the panel
-    this.summary = document.createElement('p');
-    this.node.appendChild(this.summary);
-  }
-
+  renderMarkdownCell: 'toolbar-button:render-markdown-cell',
   /**
-   * The image element associated with the widget.
+   * Command to run a code cell.
    */
-  readonly img: HTMLImageElement;
-
-  /**
-   * The summary text element associated with the widget.
-   */
-  readonly summary: HTMLParagraphElement;
-
-  /**
-   * Handle update requests for the widget.
-   */
-  async updateAPODImage(): Promise<void> {
-    const response = await fetch(
-      `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${this.randomDate()}`
-    );
-
-    if (!response.ok) {
-      const data = await response.json();
-      if (data.error) {
-        this.summary.innerText = data.error.message;
-      } else {
-        this.summary.innerText = response.statusText;
-      }
-      return;
-    }
-
-    const data = (await response.json()) as APODResponse;
-
-    if (data.media_type === 'image') {
-      // Populate the image
-      this.img.src = data.url;
-      this.img.title = data.title;
-      this.summary.innerText = data.title;
-      if (data.copyright) {
-        this.summary.innerText += ` (Copyright ${data.copyright})`;
-      }
-    } else {
-      this.summary.innerText = 'Random APOD fetched was not an image.';
-    }
-  }
-
-  /**
-   * Get a random date string in YYYY-MM-DD format.
-   */
-  randomDate(): string {
-    const start = new Date(2010, 1, 1);
-    const end = new Date();
-    const randomDate = new Date(
-      start.getTime() + Math.random() * (end.getTime() - start.getTime())
-    );
-    return randomDate.toISOString().slice(0, 10);
-  }
-}
+  runCodeCell: 'toolbar-button:run-code-cell'
+};
 
 /**
- * Activate the APOD widget extension.
+ * Activate the extension.
  */
 function activate(
   app: JupyterFrontEnd,
-  palette: ICommandPalette,
-  settingRegistry: ISettingRegistry | null,
-  restorer: ILayoutRestorer | null
+  tracker: INotebookTracker,
+  settingRegistry: ISettingRegistry | null
 ) {
-  console.log('JupyterLab extension jupyterlab_apod is activated!');
+  console.log('JupyterLab extension jupyterlab-double-sharp is activated!');
 
   if (settingRegistry) {
     settingRegistry
@@ -122,62 +46,38 @@ function activate(
       });
   }
 
-  // Declare a widget variable
-  let widget: MainAreaWidget<APODWidget>;
+  const { commands } = app;
 
-  // Add an application command
-  const command: string = 'apod:open';
-  app.commands.addCommand(command, {
-    label: 'Random Astronomy Picture',
+  /* Adds a command enabled only on code cell */
+  commands.addCommand(CommandIds.runCodeCell, {
+    icon: runIcon,
+    caption: 'Run a code cell',
     execute: () => {
-      if (!widget || widget.isDisposed) {
-        const content = new APODWidget();
-        widget = new MainAreaWidget({ content });
-        widget.id = 'apod-jupyterlab';
-        widget.title.label = 'Astronomy Picture';
-        widget.title.closable = true;
-      }
-      if (!tracker.has(widget)) {
-        // Track the state of the widget for later restoration
-        tracker.add(widget);
-      }
-      if (!widget.isAttached) {
-        // Attach the widget to the main work area if it's not there
-        app.shell.add(widget, 'main');
-      }
-
-      // Refresh the picture in the widget
-      widget.content.updateAPODImage();
-
-      // Activate the widget
-      app.shell.activateById(widget.id);
-    }
+      commands.execute('notebook:run-cell');
+    },
+    isVisible: () => tracker.activeCell?.model.type === 'code'
   });
 
-  // Add the command to the palette.
-  palette.addItem({ command, category: 'Tutorial' });
-
-  // Track and restore the widget state
-  let tracker = new WidgetTracker<MainAreaWidget<APODWidget>>({
-    namespace: 'apod'
+  /* Adds a command enabled only on markdown cell */
+  commands.addCommand(CommandIds.renderMarkdownCell, {
+    icon: markdownIcon,
+    caption: 'Render a markdown cell',
+    execute: () => {
+      commands.execute('notebook:run-cell');
+    },
+    isVisible: () => tracker.activeCell?.model.type === 'markdown'
   });
-  if (restorer) {
-    restorer.restore(tracker, {
-      command,
-      name: () => 'apod'
-    });
-  }
 }
 
 /**
  * Initialization data for the jupyterlab-double-sharp extension.
  */
 const plugin: JupyterFrontEndPlugin<void> = {
-  id: 'jupyterlab-double-sharp',
+  id: 'jupyterlab-double-sharp:plugin',
   description: 'Convert comments starting with ## to Markdown',
   autoStart: true,
-  requires: [ICommandPalette],
-  optional: [ISettingRegistry, ILayoutRestorer],
+  requires: [INotebookTracker], //[ICommandPalette],
+  optional: [ISettingRegistry],
   activate
 };
 
