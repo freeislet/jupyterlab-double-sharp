b407ffaa40c7677c1ab29d848b246ebb1df98bbc
freeislet <freeislet@gmail.com>
2024-04-16 14:00:06 +0900

    astronomy picture 노출

--- src/index.ts
+++ src/index.ts
@@ -4,20 +4,34 @@ import {
 } from '@jupyterlab/application';
 
 import { ISettingRegistry } from '@jupyterlab/settingregistry';
+import { ICommandPalette, MainAreaWidget } from '@jupyterlab/apputils';
+import { Widget } from '@lumino/widgets';
+
+interface APODResponse {
+  copyright: string;
+  date: string;
+  explanation: string;
+  media_type: 'video' | 'image';
+  title: string;
+  url: string;
+}
 
 /**
  * Initialization data for the jupyterlab-double-sharp extension.
  */
 const plugin: JupyterFrontEndPlugin<void> = {
-  id: 'jupyterlab-double-sharp:plugin',
+  id: 'jupyterlab-double-sharp',
   description: 'Convert comments starting with ## to Markdown',
   autoStart: true,
+  requires: [ICommandPalette],
   optional: [ISettingRegistry],
-  activate: (
+  activate: async (
     app: JupyterFrontEnd,
+    palette: ICommandPalette,
     settingRegistry: ISettingRegistry | null
   ) => {
     console.log('JupyterLab extension jupyterlab-double-sharp is activated!');
+    console.log('ICommandPalette:', palette);
 
     if (settingRegistry) {
       settingRegistry
@@ -35,6 +49,70 @@ const plugin: JupyterFrontEndPlugin<void> = {
           );
         });
     }
+
+    // Define a widget creator function,
+    // then call it to make a new widget
+    const newWidget = async () => {
+      // Create a blank content widget inside of a MainAreaWidget
+      const content = new Widget();
+      const widget = new MainAreaWidget({ content });
+      widget.id = 'apod-jupyterlab';
+      widget.title.label = 'Astronomy Picture';
+      widget.title.closable = true;
+
+      // 사진 보기
+      // Add an image element to the content
+      let img = document.createElement('img');
+      content.node.appendChild(img);
+
+      // Get a random date string in YYYY-MM-DD format
+      function randomDate() {
+        const start = new Date(2010, 1, 1);
+        const end = new Date();
+        const randomDate = new Date(
+          start.getTime() + Math.random() * (end.getTime() - start.getTime())
+        );
+        return randomDate.toISOString().slice(0, 10);
+      }
+
+      // Fetch info about a random picture
+      const response = await fetch(
+        `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${randomDate()}`
+      );
+      const data = (await response.json()) as APODResponse;
+
+      if (data.media_type === 'image') {
+        // Populate the image
+        img.src = data.url;
+        img.title = data.title;
+      } else {
+        console.log('Random APOD was not a picture.');
+      }
+
+      return widget;
+    };
+    let widget = await newWidget();
+
+    // Add an application command
+    const command: string = 'double-sharp:open';
+    app.commands.addCommand(command, {
+      label: 'Random Astronomy Picture',
+      execute: async () => {
+        // Regenerate the widget if disposed
+        if (widget.isDisposed) {
+          widget = await newWidget();
+        }
+        if (!widget.isAttached) {
+          // Attach the widget to the main work area if it's not there
+          app.shell.add(widget, 'main');
+        }
+        // Activate the widget
+        app.shell.activateById(widget.id);
+      }
+    });
+
+    // Add the command to the palette.
+    palette.addItem({ command, category: 'Tutorial' });
   }
 };
 
