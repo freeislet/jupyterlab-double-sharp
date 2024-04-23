3d6b25a0b7e39e620509521b657c41fdfea1b34a
freeislet <freeislet@gmail.com>
2024-04-23 13:14:18 +0900

    에디터 주석 스타일 테스트

--- src/editor.ts
+++ src/editor.ts
@@ -6,95 +6,157 @@ import {
   Decoration,
   DecorationSet,
   EditorView,
+  PluginValue,
   ViewPlugin,
   ViewUpdate
 } from '@codemirror/view';
-import { Extension, Facet, RangeSetBuilder } from '@codemirror/state';
+// import { Extension, Facet, RangeSetBuilder } from '@codemirror/state';
+import { RangeSetBuilder } from '@codemirror/state';
+import { syntaxTree } from '@codemirror/language';
 
-// Defines new styles for this extension
-const baseTheme = EditorView.baseTheme({
-  // We need to set some transparency because the stripe are above
-  // the selection layer
-  '&light .cm-zebraStripe': { backgroundColor: '#d4fafaaa' },
-  '&dark .cm-zebraStripe': { backgroundColor: '#1a2727aa' }
+// styles
+const commentBaseTheme = EditorView.baseTheme({
+  '.cm-commentLine': { backgroundColor: '#bababa1a' }
 });
 
-// Resolve step to use in the editor
-const stepSize = Facet.define<number, number>({
-  combine: values => (values.length ? Math.min(...values) : 2)
+// line decoration
+const commentDecoration = Decoration.line({
+  attributes: { class: 'cm-commentLine' }
 });
 
-// Add decoration to editor lines
-const stripe = Decoration.line({
-  attributes: { class: 'cm-zebraStripe' }
-});
+// view plugin
+class CommentPlugin implements PluginValue {
+  decorations: DecorationSet;
+  firstUpdate = true; // 문서 생성 update 시 docChanged false 이슈 우회
 
-// Create the range of lines requiring decorations
-function stripeDeco(view: EditorView) {
-  const step = view.state.facet(stepSize) as number;
-  const builder = new RangeSetBuilder<Decoration>();
-  for (const { from, to } of view.visibleRanges) {
-    for (let pos = from; pos <= to; ) {
-      const line = view.state.doc.lineAt(pos);
-      if (line.number % step === 0) {
-        builder.add(line.from, line.from, stripe);
-      }
-      pos = line.to + 1;
+  constructor(view: EditorView) {
+    this.decorations = this.commentDeco(view);
+  }
+
+  update(update: ViewUpdate) {
+    if (update.docChanged || update.viewportChanged || this.firstUpdate) {
+      this.decorations = this.commentDeco(update.view);
+      this.firstUpdate = false;
     }
   }
-  return builder.finish();
-}
 
-// Update the decoration status of the editor view
-const showStripes = ViewPlugin.fromClass(
-  class {
-    decorations: DecorationSet;
-
-    constructor(view: EditorView) {
-      this.decorations = stripeDeco(view);
+  commentDeco(view: EditorView) {
+    const builder = new RangeSetBuilder<Decoration>();
+    for (const { from, to } of view.visibleRanges) {
+      syntaxTree(view.state).iterate({
+        from,
+        to,
+        enter: node => {
+          if (node.name == 'Comment') {
+            builder.add(node.from, node.from, commentDecoration);
+          }
+        }
+      });
     }
-
-    update(update: ViewUpdate) {
-      // Update the stripes if the document changed,
-      // the viewport changed or the stripes step changed.
-      const oldStep = update.startState.facet(stepSize);
-      if (
-        update.docChanged ||
-        update.viewportChanged ||
-        oldStep !== update.view.state.facet(stepSize)
-      ) {
-        this.decorations = stripeDeco(update.view);
-      }
-    }
-  },
-  {
-    decorations: v => v.decorations
+    return builder.finish();
   }
-);
-
-// Full extension composed of elemental extensions
-export function zebraStripes(options: { step?: number } = {}): Extension {
-  return [
-    baseTheme,
-    typeof options.step !== 'number' ? [] : stepSize.of(options.step),
-    showStripes
-  ];
 }
 
-export const editorExtensionFactory: IEditorExtensionFactory<number> =
-  Object.freeze({
-    name: 'jupyterlab-double-sharp:zebra-stripes',
-    // Default CodeMirror extension parameters
-    default: 2,
-    factory: (options: IEditorExtensionFactory.IOptions) =>
-      // The factory will be called for every new CodeMirror editor
-      EditorExtensionRegistry.createConfigurableExtension((step: number) =>
-        zebraStripes({ step })
-      ),
-    // JSON schema defining the CodeMirror extension parameters
-    schema: {
-      type: 'number',
-      title: 'Show stripes',
-      description: 'Display zebra stripes every "step" in CodeMirror editors.'
-    }
-  });
+const commentPlugin = ViewPlugin.fromClass(CommentPlugin, {
+  decorations: v => v.decorations
+});
+
+// TODO: CommentHeadingParameters
+// interface EditorExtensionParameters {
+//   step: number;
+// }
+
+export const editorExtensionFactory: IEditorExtensionFactory = Object.freeze({
+  name: 'jupyterlab-double-sharp:comment-parser',
+  factory: (options: IEditorExtensionFactory.IOptions) =>
+    EditorExtensionRegistry.createImmutableExtension(
+      options.model.mimeType === 'text/x-ipython' ||
+        options.model.mimeType === 'text/x-python'
+        ? [commentBaseTheme, commentPlugin]
+        : []
+    )
+});
+
+// const baseTheme = EditorView.baseTheme({
+//   '&light .cm-zebraStripe': { backgroundColor: '#d4fafa' },
+//   '&dark .cm-zebraStripe': { backgroundColor: '#1a2727' }
+// });
+
+// const stripeDecoration = Decoration.line({
+//   attributes: { class: 'cm-zebraStripe' }
+// });
+
+// // Resolve step to use in the editor
+// const stepSizeFacet = Facet.define<number, number>({
+//   combine: values => (values.length ? Math.min(...values) : 2)
+// });
+
+// // plugin
+// class StripePlugin implements PluginValue {
+//   decorations: DecorationSet;
+
+//   constructor(view: EditorView) {
+//     this.decorations = this.stripeDeco(view);
+//   }
+
+//   update(update: ViewUpdate) {
+//     // Update the stripes if the document changed,
+//     // the viewport changed or the stripes step changed.
+//     const oldStep = update.startState.facet(stepSizeFacet);
+//     if (
+//       update.docChanged ||
+//       update.viewportChanged ||
+//       oldStep !== update.view.state.facet(stepSizeFacet)
+//     ) {
+//       this.decorations = this.stripeDeco(update.view);
+//     }
+//   }
+
+//   // Create the range of lines requiring decorations
+//   stripeDeco(view: EditorView) {
+//     const step = view.state.facet(stepSizeFacet) as number;
+//     const builder = new RangeSetBuilder<Decoration>();
+//     for (const { from, to } of view.visibleRanges) {
+//       for (let pos = from; pos <= to; ) {
+//         const line = view.state.doc.lineAt(pos);
+//         if (line.number % step === 0) {
+//           builder.add(line.from, line.from, stripeDecoration);
+//         }
+//         pos = line.to + 1;
+//       }
+//     }
+//     return builder.finish();
+//   }
+// }
+
+// const stripePlugin = ViewPlugin.fromClass(StripePlugin, {
+//   decorations: v => v.decorations
+// });
+
+// // Full extension composed of elemental extensions
+// export function editorExtension(options: { step?: number } = {}): Extension {
+//   return [
+//     baseTheme,
+//     typeof options.step !== 'number' ? [] : stepSizeFacet.of(options.step),
+//     stripePlugin
+//   ];
+// }
+
+// export const editorExtensionFactory: IEditorExtensionFactory<number> =
+//   Object.freeze({
+//     name: 'jupyterlab-double-sharp:zebra-stripes',
+//     // Default CodeMirror extension parameters
+//     default: 2,
+//     factory: (options: IEditorExtensionFactory.IOptions) =>
+//       // The factory will be called for every new CodeMirror editor
+//       EditorExtensionRegistry.createConfigurableExtension((step: number) =>
+//         editorExtension({ step })
+//       ),
+//     // JSON schema defining the CodeMirror extension parameters
+//     // - 참고: https://github.com/jupyterlab/jupyterlab/blob/2ceabd8c7b98e7b2c305be406ade799f387a90eb/packages/codemirror/src/extension.ts#L971
+//     schema: {
+//       type: 'number',
+//       title: 'Show stripes',
+//       description: 'Display zebra stripes every "step" in CodeMirror editors.'
+//     }
+//   });
