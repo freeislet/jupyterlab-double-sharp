83466876e2646fe50c7ea6aa2497e1e85866e422
freeislet <freeislet@gmail.com>
2024-04-30 02:06:00 +0900

    document widget 테스트 코드 제거

--- src/cell/index.ts
+++ src/cell/index.ts
@@ -1,11 +1,9 @@
 import { JupyterFrontEnd } from '@jupyterlab/application';
 
 import { CellExtension } from './tracker';
-// import { WidgetExtension } from './notebook';
 
 export function setupCellExtensions(app: JupyterFrontEnd) {
   app.docRegistry.addWidgetExtension('Notebook', new CellExtension());
-  // app.docRegistry.addWidgetExtension('Notebook', new WidgetExtension());
 }
 
 export * from './actions';
