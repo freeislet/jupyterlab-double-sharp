13d6aee2a4d07679d3111e339c065ec80c0055f0
freeislet <freeislet@gmail.com>
2024-05-06 22:50:26 +0900

    cs magic tokenize 시 ' 따옴표도 사용할 수 있도록 수정

--- src/cs-magic/utils.ts
+++ src/cs-magic/utils.ts
@@ -1,7 +1,7 @@
 export function tokenize(command: string): string[] {
   const tokens: string[] = [];
   // NOTE: "a b c"를 하나의 토큰으로 처리 (결과에 "는 제외)
-  const matches = command.matchAll(/"([^"]*)"|\S+/g);
+  const matches = command.matchAll(/"([^"]*)"|'([^']*)'|\S+/g);
   for (const match of matches) {
     const capturedOrMatched = match[1] || match[0];
     tokens.push(capturedOrMatched);
