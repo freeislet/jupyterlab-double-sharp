a85ea22be83b75292d0a151b6344011da5eb0114
freeislet <freeislet@gmail.com>
2024-05-16 15:30:07 +0900

    variable.ICellVariables export

--- src/variable/index.ts
+++ src/variable/index.ts
@@ -1,9 +1,9 @@
 import { JupyterFrontEnd } from '@jupyterlab/application';
 
-import { VariableExtension, VariableTracker } from './tracker';
+import { VariableExtension, VariableTracker, ICellVariables } from './tracker';
 
 export function setupVariableExtensions(app: JupyterFrontEnd) {
   app.docRegistry.addWidgetExtension('Notebook', new VariableExtension());
 }
 
-export { VariableTracker };
+export { VariableTracker, ICellVariables };
