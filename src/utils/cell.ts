d9509a51722d6e60da6c1c8feaa8ffb879db64da
freeislet <freeislet@gmail.com>
2024-05-21 00:32:06 +0900

    getAboveCells

--- src/utils/cell.ts
+++ src/utils/cell.ts
@@ -1,5 +1,17 @@
 import { Cell, CodeCell, isCodeCellModel } from '@jupyterlab/cells';
+import { Notebook } from '@jupyterlab/notebook';
 
 export function isCodeCell(cell: Cell): cell is CodeCell {
   return isCodeCellModel(cell.model);
+}
+
+export function getAboveCells(cell: Cell): Cell[] {
+  const notebook = cell.parent as Notebook;
+  if (!notebook) return [];
+
+  const index = notebook.widgets.findIndex(c => c === cell);
+  if (index < 0) return [];
+
+  const aboves = notebook.widgets.slice(0, index);
+  return aboves;
 }
