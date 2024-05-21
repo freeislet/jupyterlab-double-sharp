d5113745c8a370962d5b61cbaf3fd6d4b007871f
freeislet <freeislet@gmail.com>
2024-05-21 10:02:51 +0900

    Cache initialValue 추가

--- src/utils/cache.ts
+++ src/utils/cache.ts
@@ -1,7 +1,12 @@
 export class Cache<T> {
   private _value?: T;
 
-  constructor(private readonly setter: () => T) {}
+  constructor(
+    private readonly setter: () => T,
+    initialValue?: T
+  ) {
+    this._value = initialValue;
+  }
 
   get value(): T {
     if (!this._value) {
