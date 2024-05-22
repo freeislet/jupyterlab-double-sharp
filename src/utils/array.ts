b89604d7f0e8d14c3d2f43d62363e82ec91eeb84
freeislet <freeislet@gmail.com>
2024-05-22 09:52:33 +0900

    In, notIn predicate에 Set 반영

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -1,19 +1,27 @@
 /**
  * Array.filter predicate
  * Usage: arrA.filter(In(arrB))
- * @param array array
+ * @param list Array or Set
  * @returns filter function
  */
-export function In<T>(array: Array<T>): (item: T) => boolean {
-  return (item: T) => array.includes(item);
+export function In<T>(list: Array<T> | Set<T>): (item: T) => boolean {
+  if (Array.isArray(list)) {
+    return (item: T) => list.includes(item);
+  } else if (list instanceof Set) {
+    return (item: T) => list.has(item);
+  } else throw new TypeError();
 }
 
 /**
  * Array.filter predicate
  * Usage: arrA.filter(NotIn(arrB))
- * @param array array
+ * @param list Array or Set
  * @returns filter function
  */
-export function notIn<T>(array: Array<T>): (item: T) => boolean {
-  return (item: T) => !array.includes(item);
+export function notIn<T>(list: Array<T> | Set<T>): (item: T) => boolean {
+  if (Array.isArray(list)) {
+    return (item: T) => !list.includes(item);
+  } else if (list instanceof Set) {
+    return (item: T) => !list.has(item);
+  } else throw new TypeError();
 }
