9776b19107476e524eb71e9c9386e89d34ab4bde
freeislet <freeislet@gmail.com>
2024-06-18 16:52:25 +0900

    inspector settings

--- src/inspector/components/settings.tsx
+++ src/inspector/components/settings.tsx
@@ -1,17 +1,79 @@
 import * as React from 'react';
+import merge from 'lodash.merge';
 
+import { Settings as AppSettings } from '../../settings';
+import { useSignal } from '../../ui/hooks/signal';
+import Group from '../../ui/group';
 import Checkbox from '../../ui/checkbox';
 
 // export interface ISettingsProps {}
 
 export default function Settings() {
+  const [execution, setExecution] = React.useState(AppSettings.data.execution);
+  useSignal(AppSettings.executionChanged, (_, change) => {
+    setExecution(change.newValue);
+  });
+
+  function updateExecution(partial: Partial<AppSettings.IExecution>) {
+    AppSettings.updateExecution(partial);
+    setExecution(execution => merge({}, execution, partial)); // NOTE: rerender 위해 {} 생성
+  }
+
+  const onUseCache = React.useCallback(
+    (checked: boolean) => updateExecution({ useCache: checked }),
+    []
+  );
+  const onAutoDependency = React.useCallback(
+    (checked: boolean) => updateExecution({ autoDependency: checked }),
+    []
+  );
+  const onForceSingleExec = React.useCallback(
+    (checked: boolean) =>
+      updateExecution({ forceExecutionOnSingleCell: checked }),
+    []
+  );
+
+  // Log.debug('Settings', execution, AppSettings.data.execution);
+
   return (
-    <Checkbox
-      className="jp-DoubleSharp-Settings-row"
-      checked={true}
-      // onChange={e => onChange(e.target.checked)}
-    >
-      settings...
-    </Checkbox>
+    <Group>
+      <Group.Title>Execution settings</Group.Title>
+      <BooleanSetting checked={execution.useCache} onChange={onUseCache}>
+        Use Cache by default
+      </BooleanSetting>
+      <BooleanSetting
+        checked={execution.autoDependency}
+        onChange={onAutoDependency}
+      >
+        Auto Dependency by default
+      </BooleanSetting>
+      <BooleanSetting
+        checked={execution.forceExecutionOnSingleCell}
+        onChange={onForceSingleExec}
+      >
+        Force Execution on Single Cell
+      </BooleanSetting>
+    </Group>
   );
 }
+
+interface IBooleanSettingProps {
+  checked: boolean;
+  onChange: (checked: boolean) => void;
+  children?: React.ReactNode;
+}
+
+const BooleanSetting = React.memo(
+  ({ checked, onChange, children }: IBooleanSettingProps) => {
+    return (
+      <Checkbox
+        className="jp-DoubleSharp-Settings-row"
+        checked={checked}
+        onChange={e => onChange(e.target.checked)}
+      >
+        <span>{children}</span>
+      </Checkbox>
+    );
+  }
+);
+BooleanSetting.displayName = 'BooleanSetting';
