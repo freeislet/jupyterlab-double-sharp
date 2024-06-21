b55160c6b2aac6e622ddfd0516ecb34bd069f746
freeislet <freeislet@gmail.com>
2024-06-21 15:44:48 +0900

    CellContext.isCodeCell 추가

--- src/cell/context.ts
+++ src/cell/context.ts
@@ -2,12 +2,17 @@ import { Cell } from '@jupyterlab/cells';
 
 import { CodeContext } from './code';
 import { Cache } from '../utils/cache';
+import { isCodeCell } from '../utils/cell';
 
 export class CellContext {
   private _codeContextCache: Cache<CodeContext | undefined>;
 
   get codeContext(): CodeContext | undefined {
     return this._codeContextCache.value;
+  }
+
+  get isCodeCell(): boolean {
+    return isCodeCell(this.cell);
   }
 
   //----
