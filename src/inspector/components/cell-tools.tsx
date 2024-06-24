d478e051e1bafc0a035307142b2b253db17ace2e
freeislet <freeislet@gmail.com>
2024-06-24 16:52:22 +0900

    Cell Inspector Code에 Context 반영, settings/metadata changed 처리

--- src/inspector/components/cell-tools.tsx
+++ src/inspector/components/cell-tools.tsx
@@ -1,8 +1,7 @@
 import * as React from 'react';
-import { ICellModel } from '@jupyterlab/cells';
-import { IMapChange } from '@jupyter/ydoc';
 
 import { CellContext, CellMetadata, CellConfig } from '../../cell';
+import { Settings } from '../../settings';
 import { useStateObject, useSignal } from '../../ui/hooks';
 import Group from '../../ui/group';
 import Checkbox, { NullableCheckbox } from '../../ui/checkbox';
@@ -30,6 +29,11 @@ interface ICodeCellContext {
   context: CellContext;
   config: CellConfig.IConfig;
   updateConfig: (config: Partial<CellConfig.IConfig>) => void;
+  compositeConfig: NonNullableField<CellConfig.IConfig>;
+  compositeConfigDirty: boolean;
+  code: CellMetadata.ICode | undefined;
+  codeDirty: boolean;
+  updateCode: () => void;
 }
 
 const CodeCellContext = React.createContext<ICodeCellContext | undefined>(
@@ -49,6 +53,7 @@ interface IContextProps {
 function CodeCellTools({ context }: IContextProps) {
   const model = context.cell.model;
 
+  // config state, callback
   const [config, setConfig, updateConfig] = useStateObject(
     CellMetadata.config.defaultValue
   );
@@ -60,28 +65,93 @@ function CodeCellTools({ context }: IContextProps) {
     [context]
   );
 
-  React.useEffect(() => {
-    const config = CellMetadata.config.getCoalesced(model);
-    setConfig(config);
-    Log.debug('Config', model.id, config);
+  // settings, cell config 통합 config states
+  const getCompositeConfig = () => CellConfig.get(model, false); // checkDirty=false: cs-command까지는 확인 안 함
+  const [compositeConfig, setCompositeConfig] =
+    React.useState<NonNullableField<CellMetadata.IConfig>>(getCompositeConfig);
+  const [compositeConfigDirty, setCompositeConfigDirty] = React.useState(false); // cs-command dirty
+
+  // code states, callback
+  const [code, setCode] = React.useState<CellMetadata.ICode>();
+  const [codeDirty, setCodeDirty] = React.useState(false);
+  const updateCode = React.useCallback(() => {
+    const update = async () => {
+      await context.codeContext?.getData();
+      // NOTE: state 업데이트 필요 없음 -> metadataChanged signal에서 처리
+    };
+    update();
   }, [context]);
 
+  // context 초기화
+  React.useEffect(() => {
+    setConfig(CellMetadata.config.getCoalesced(model));
+    setCompositeConfig(getCompositeConfig());
+    setCompositeConfigDirty(CellMetadata.configOverride.isDirty(model));
+    setCode(CellMetadata.code.get(model, false));
+    setCodeDirty(CellMetadata.code.isDirty(model));
+  }, [context]);
+
+  // Settings.executionChanged 시 state 업데이트
+  useSignal(
+    Settings.executionChanged,
+    (_, change) => {
+      // Execution 세팅 변경
+      setCompositeConfig(getCompositeConfig());
+    },
+    [context]
+  );
+
+  // metadataChanged 시 state 업데이트
   useSignal(
     model.metadataChanged,
-    (model: ICellModel, change: IMapChange) => {
-      if (change.key === CellMetadata.configOverride.dirtyFlagName) {
-        // TODO
+    (model, change) => {
+      switch (change.key) {
+        case CellMetadata.config.name: // ##Config
+        // Cell Inspector > Config > updateConfig
+        case CellMetadata.configOverride.name: // ##ConfigOverride
+          // 셀 실행 > Client-side Magic Command
+          setCompositeConfig(getCompositeConfig());
+          break;
+
+        case CellMetadata.configOverride.dirtyFlagName: // ##ConfigOverride-dirty
+          // 셀 실행 시 dirty 해결 (false)
+          // source 수정 시 dirty 설정 (true)
+          setCompositeConfigDirty(change.newValue as boolean);
+          break;
+
+        case CellMetadata.code.name: // ##Code
+          // 1. dirty 알림 "click" 클릭 > updateCode > CodeContext.getData
+          // 2. 셀 실행 (compositeConfig.cache or autoDependency true 시)
+          setCode(change.newValue);
+          break;
+
+        case CellMetadata.code.dirtyFlagName: // ##Code-dirty
+          // ##Code 업데이트 시 dirty 해결 (false)
+          // source 수정 시 dirty 설정 (true)
+          setCodeDirty(change.newValue as boolean);
+          break;
       }
+
+      // Log.debug('Code metadataChanged', model, change);
     },
     [context]
   );
 
   return (
     <CodeCellContext.Provider
-      value={{ context, config, updateConfig: updateAndApplyConfig }}
+      value={{
+        context,
+        config,
+        updateConfig: updateAndApplyConfig,
+        compositeConfig,
+        compositeConfigDirty,
+        code,
+        codeDirty,
+        updateCode
+      }}
     >
       <Config />
-      <Code context={context} />
+      <Code />
       {context.cell.model.id}
     </CodeCellContext.Provider>
   );
@@ -165,60 +235,18 @@ function NullableBooleanConfig({
 /**
  * Code
  */
-function Code({ context }: IContextProps) {
-  const [metadata, setMetadata] = React.useState<
-    CellMetadata.ICode | undefined
-  >(CellMetadata.code.defaultValue);
-  const [dirty, setDirty] = React.useState(false);
-  const [executionMatters, setExecutionMatters] = React.useState(false);
+function Code() {
+  const { code, codeDirty, updateCode, compositeConfig } =
+    React.useContext(CodeCellContext)!;
 
-  const model = context.cell.model;
-
-  React.useEffect(() => {
-    const metadata = CellMetadata.code.get(model, false);
-    const dirty = CellMetadata.code.isDirty(model);
-    setMetadata(metadata);
-    setDirty(dirty);
-
-    // 실행을 통해 dirty 상태 해결되는지 여부에 따라 "execute the cell" 문구 표시
-    const config = CellConfig.get(model, false); // checkDirty=false: cs-command까지는 확인 안 함
-    Log.debug('Code executionMatters', model, config);
-    setExecutionMatters(config.cache || config.autoDependency);
-  }, [context]);
-
-  useSignal(
-    model.metadataChanged,
-    (model: ICellModel, change: IMapChange) => {
-      // metadataChanged signal을 통해 metadata, dirty state 갱신
-      //   1. dirty 해결 (false)
-      //     A. 셀 실행
-      //     B. dirty 알림 click 클릭 > update > CodeContext.getData
-      //   2. dirty 설정 (true)
-      //     A. source 수정
-
-      switch (change.key) {
-        case CellMetadata.code.name: // ##Code
-          setMetadata(change.newValue);
-          break;
-
-        case CellMetadata.code.dirtyFlagName: // ##Code-dirty
-          setDirty(change.newValue as boolean);
-          break;
-      }
-
-      // Log.debug('Code metadataChanged', model, change);
-    },
-    [context]
-  );
-
-  const updateMetadata = async () => {
-    await context.codeContext?.getData();
-  };
+  // 실행을 통해 dirty 상태 해결되는지 여부에 따라 "execute the cell" 문구 표시
+  const executionMatters =
+    compositeConfig.cache || compositeConfig.autoDependency;
 
   return (
     <Group>
       <Group.Title>Code</Group.Title>
-      {dirty && (
+      {codeDirty && (
         <Block type="warning" className="jp-DoubleSharp-Inspector-row">
           Code info may be invalid.
           <br />
@@ -227,16 +255,16 @@ function Code({ context }: IContextProps) {
               <strong>execute</strong> the cell or{' '}
             </>
           )}
-          <a onClick={updateMetadata}>click</a> to update.
+          <a onClick={updateCode}>click</a> to update.
         </Block>
       )}
       <div className="jp-DoubleSharp-Inspector-row jp-DoubleSharp-Inspector-space">
         <strong>Variables: </strong>
-        <span>{metadata?.variables.join(', ')}</span>
+        <span>{code?.variables.join(', ')}</span>
       </div>
       <div className="jp-DoubleSharp-Inspector-row jp-DoubleSharp-Inspector-space">
         <strong>Unbound Vars: </strong>
-        <span>{metadata?.unboundVariables.join(', ')}</span>
+        <span>{code?.unboundVariables.join(', ')}</span>
       </div>
     </Group>
   );
