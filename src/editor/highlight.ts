b32d1c24a56e7a5dfba6ed3d8e7af465c8b55981
freeislet <freeislet@gmail.com>
2024-07-04 11:21:08 +0900

    codemirror extension 코드 정리, compartment 적용 (wip)

--- src/editor/highlight.ts
+++ src/editor/highlight.ts
@@ -3,7 +3,10 @@ import {
   EditorExtensionRegistry,
   IEditorExtensionFactory,
   IConfigurableExtension
+  // CodeMirrorEditor
 } from '@jupyterlab/codemirror';
+import { CodeEditor } from '@jupyterlab/codeeditor';
+// import { Cell } from '@jupyterlab/cells';
 import {
   Decoration,
   DecorationSet,
@@ -18,6 +21,7 @@ import { SyntaxNodeRef } from '@lezer/common';
 
 import { ConfigFacet } from '../utils/editor';
 import { Settings } from '../settings';
+// import { CellActions } from '../cell';
 
 const FACTORY_NAME = 'jupyterlab-double-sharp:editor-highlight';
 
@@ -83,6 +87,12 @@ const highlightPlugin = ViewPlugin.fromClass(HighlightPlugin, {
   decorations: v => v.decorations
 });
 
+// filter (plugin 적용 조건)
+
+function filter(model: CodeEditor.IModel | null | undefined): boolean {
+  return model?.mimeType === 'text/x-ipython';
+}
+
 // settings
 
 function applySettings(
@@ -95,65 +105,60 @@ function applySettings(
   for (const handler of handlers) {
     handler.setOption(FACTORY_NAME, editorSettings);
   }
-  // console.log('registry.baseConfiguration', registry.baseConfiguration);
-  // (registry as EditorExtensionRegistry).baseConfiguration = {
-  //   FACTORY_NAME: editor
-  // };
+  // CellActions.forAllCells(
+  //   (cell: Cell) => {
+  //     const view = (cell.editor as CodeMirrorEditor).editor;
+  //     highlightConfig.apply(view, editorSettings.highlight);
+  //   },
+  //   (cell: Cell) => filter(cell.editor?.model)
+  // );
 }
 
 // extension
 
-// NOTE: CodeMirror 세팅 화면이 아닌 Double Sharp 세팅에 표시하기 위해 plugin.json에 추가
-// const schema = {
-//   title: 'Double Sharp Editor Options',
-//   type: 'object',
-//   properties: {
-//     highlight: {
-//       title: 'Highlight ## lines',
-//       type: 'boolean'
-//       // description: '...'
-//     }
-//   }
-// };
-
-// interface IHighlightParams {
-//   highlight?: boolean;
-// }
-type IHighlightParams = Settings.IEditor;
-
-function createEditorExtension(
-  options: IEditorExtensionFactory.IOptions
-): IConfigurableExtension<IHighlightParams> | null {
-  const valid = options.model.mimeType === 'text/x-ipython';
-  if (!valid) return null;
-
-  return EditorExtensionRegistry.createConfigurableExtension(
-    (params: IHighlightParams) => [
-      // highlightConfig.instance(params.highlight ?? false),
-      // NOTE: extension과 연결된 EditorView를 참조할 수 없어서 그냥 facet 사용 (단, schema 등록 X)
-      // TODO: 전체 EditorView 참조 방식 검토 (NotebookCellActions.forAllCells 같은 방식?)
-      highlightConfig.facet.of(params.highlight ?? false),
-      highlightTheme,
-      highlightPlugin
-    ]
-  );
-}
-
-const factory: IEditorExtensionFactory<IHighlightParams> = Object.freeze({
-  name: FACTORY_NAME,
-  factory: createEditorExtension,
-  default: { highlight: true }
-  // schema // NOTE: CodeMirror 세팅 화면이 아닌 Double Sharp 세팅에 표시하기 위해 schema 주석 처리
-});
-
 export function setupHighlightExtension(registry: IEditorExtensionRegistry) {
-  // applySettings(Settings.settings.editor, registry);
+  // interface IHighlightParams {
+  //   highlight?: boolean;
+  // }
+  type IHighlightParams = Settings.IEditor;
+
+  const factory: IEditorExtensionFactory<IHighlightParams> = Object.freeze({
+    name: FACTORY_NAME,
+    factory: (
+      options: IEditorExtensionFactory.IOptions
+    ): IConfigurableExtension<IHighlightParams> | null => {
+      if (!filter(options.model)) return null;
+
+      return EditorExtensionRegistry.createConfigurableExtension(
+        (params: IHighlightParams) => [
+          // highlightConfig.instance(params.highlight ?? false),
+          // NOTE: extension과 연결된 EditorView를 참조할 수 없어서 그냥 facet 사용 (단, schema 등록 X)
+          // TODO: 전체 EditorView 참조 방식 검토 (NotebookCellActions.forAllCells 같은 방식?)
+          highlightConfig.facet.of(params.highlight ?? false),
+          highlightTheme,
+          highlightPlugin
+        ]
+      );
+    },
+    default: { highlight: true }
+    // NOTE: CodeMirror 세팅이 아닌 Double Sharp 세팅에 표시하기 위해 schema 대신 plugin.json에 추가
+    // schema: {
+    //   title: 'Double Sharp Editor Options',
+    //   type: 'object',
+    //   properties: {
+    //     highlight: {
+    //       title: 'Highlight ## lines',
+    //       type: 'boolean'
+    //     }
+    //   }
+    // }
+  });
+  registry.addExtension(factory);
 
   Settings.editorChanged.connect(
     (settings: Settings, change: Settings.IChangeParams<Settings.IEditor>) => {
       applySettings(registry, change.newValue);
     }
   );
-
-  registry.addExtension(factory);
+  // applySettings(Settings.settings.editor, registry);
 }
