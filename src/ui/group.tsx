04ff81a5f2b1e5883272ab5e82de8caa7cb68253
freeislet <freeislet@gmail.com>
2024-07-23 09:45:37 +0900

    Group component 코드 정리

--- src/ui/group.tsx
+++ src/ui/group.tsx
@@ -1,25 +1,23 @@
 import * as React from 'react';
 import cn from 'classnames';
 
-import { IChildrenProps } from '.';
+import { IDivProps } from '.';
 
-export interface IGroupProps
-  extends React.HTMLAttributes<HTMLDivElement>,
-    IChildrenProps {}
+export interface IGroupProps extends IDivProps {}
 
-export default function Group({ className, children }: IGroupProps) {
+export default function Group({ className, children, ...props }: IGroupProps) {
   return (
-    <div className={cn('jp-DoubleSharp-Group', className)}>{children}</div>
+    <div className={cn('jp-DoubleSharp-Group', className)} {...props}>
+      {children}
+    </div>
   );
 }
 
-export interface IGroupTitleProps
-  extends React.HTMLAttributes<HTMLDivElement>,
-    IChildrenProps {}
+export interface IGroupTitleProps extends IDivProps {}
 
-function GroupTitle({ className, children }: IGroupTitleProps) {
+function GroupTitle({ className, children, ...props }: IGroupTitleProps) {
   return (
-    <div className={cn('jp-DoubleSharp-Group-title', className)}>
+    <div className={cn('jp-DoubleSharp-Group-title', className)} {...props}>
       {children}
     </div>
   );
